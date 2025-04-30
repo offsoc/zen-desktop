5fe326aad8826fa29269cb22361190c3d8d31f1e
mr. m <mr.m@tuta.com>
2025-04-30 20:12:44 +0200

    chore: Add support for 9 essentials dynamic layout, b=(no-bug), c=workspaces

--- src/zen/workspaces/ZenWorkspaces.mjs
+++ src/zen/workspaces/ZenWorkspaces.mjs
@@ -2396,7 +2396,7 @@ var ZenWorkspaces = new (class extends ZenMultiWindowFeature {
         const essentialContainer = this.getEssentialsSection(workspaceObject.containerTabId);
         const essentialNumChildren = essentialContainer.children.length;
         let essentialHackType = 0;
-        if (essentialNumChildren === 6) {
+        if (essentialNumChildren === 6 || essentialNumChildren === 9) {
           essentialHackType = 1;
         } else if (essentialNumChildren % 2 === 0) {
           essentialHackType = 2;
