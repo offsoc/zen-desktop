3b4f96ab2f82b4858f1974aee2ea371b65bd124f
Mr. M <mr.m@tuta.com>
2025-05-10 15:00:22 +0200

    test: Added tests for https://github.com/zen-browser/desktop/issues/7385, b=(closes #7385), c=tests, vendor

--- src/zen/tests/urlbar/head.js
+++ src/zen/tests/urlbar/head.js
@@ -9,3 +9,29 @@ function simulateClick(win = window) {
   EventUtils.synthesizeMouseAtCenter(target, {});
   return promise;
 }
+
+function selectWithMouseDrag(fromX, toX, win = window) {
+  let target = win.gURLBar.inputField;
+  let rect = target.getBoundingClientRect();
+  let promise = BrowserTestUtils.waitForEvent(target, 'mouseup');
+  EventUtils.synthesizeMouse(target, fromX, rect.height / 2, { type: 'mousemove' }, target.ownerGlobal);
+  EventUtils.synthesizeMouse(target, fromX, rect.height / 2, { type: 'mousedown' }, target.ownerGlobal);
+  EventUtils.synthesizeMouse(target, toX, rect.height / 2, { type: 'mousemove' }, target.ownerGlobal);
+  EventUtils.synthesizeMouse(target, toX, rect.height / 2, { type: 'mouseup' }, target.ownerGlobal);
+  return promise;
+}
+
+function goToMultipleLayouts(callback) {
+  return new Promise(async (resolve) => {
+    await SpecialPowers.pushPrefEnv({
+      set: [['zen.view.use-single-toolbar', false]],
+    });
+    setTimeout(async () => {
+      await callback();
+      await SpecialPowers.popPrefEnv();
+      setTimeout(() => {
+        resolve();
+      }, 1000); // Wait for new layout
+    }, 1000); // Wait for new layout
+  });
+}
