e0ac9ba4247604fc0bb1aab23377e6b99219961d
Mr. M <mr.m@tuta.com>
2025-05-25 17:15:19 +0200

    chore: Added licenses to remanining files, b=(no-bug), c=common, compact-mode, folders, glance, media, mods, tabs, tests, workspaces

--- src/zen/common/ZenCustomizableUI.sys.mjs
+++ src/zen/common/ZenCustomizableUI.sys.mjs
@@ -1,4 +1,6 @@
-import { AppConstants } from 'resource://gre/modules/AppConstants.sys.mjs';
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 export var ZenCustomizableUI = new (class {
   constructor() {}
