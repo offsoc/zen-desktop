8bd7f187a3c0b95072b59b45afbc2494f976d5eb
mr. m <mr.m@tuta.com>
2025-09-06 17:24:37 +0200

    fix: Fixed bookmarks duplicating at startup, b=closes #10272, c=common, workspaces

--- src/zen/workspaces/ZenWorkspaces.mjs
+++ src/zen/workspaces/ZenWorkspaces.mjs
@@ -926,7 +926,6 @@ var gZenWorkspaces = new (class extends nsZenMultiWindowFeature {
     await this.initializeTabsStripSections();
     this._initializeEmptyTab();
     await gZenPinnedTabManager.refreshPinnedTabs({ init: true });
-    this._invalidateBookmarkContainers();
     await this.changeWorkspace(activeWorkspace, { onInit: true });
     this.#fixTabPositions();
     this.onWindowResize();
