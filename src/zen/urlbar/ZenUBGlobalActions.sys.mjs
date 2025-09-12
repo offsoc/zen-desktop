9ce963f1cd88f4a2198d785e399cb2b833a47686
mr. m <91018726+mr-cheffy@users.noreply.github.com>
2025-09-13 01:35:38 +0200

    feat: New urlbar actions, c=common, p=#10340

---
+++ src/zen/urlbar/ZenUBGlobalActions.sys.mjs
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+export const globalActions = [
+  {
+    label: 'Toggle Compact Mode',
+    command: 'cmd_zenCompactModeToggle',
+    icon: 'chrome://browser/skin/zen-icons/sidebar.svg',
+  },
+  {
+    label: 'Open Theme Picker',
+    command: 'cmd_zenOpenZenThemePicker',
+    icon: 'chrome://browser/skin/zen-icons/edit-theme.svg',
+    suggestedIndex: 4,
+  },
+];
